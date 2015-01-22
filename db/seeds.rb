eca9e7d1d9d20086fb2244aebed54d74de1eb35a
Susan <susan@marpy.net>
2015-01-21 22:05:04 -0500

    created basic cars app with script text and image in doc

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+1.upto(10) do |x|
+  Car.create(make: "Car_#{x}", model: "Model_#{x ** 3}", year: rand(1900..2015))
+end
